1adc301636288f8b385c21b6936b0628e1f8f842
Shamnas <shamnasp@ziyaacademy.co.in>
2026-01-01 09:55:16 +0530

    pull on newyear

--- src/viewmodels/TaskViewModel2.ts
+++ src/viewmodels/TaskViewModel2.ts
@@ -1,14 +1,15 @@
 import { Task } from "../models/Task";
 
 export class TaskViewModel2 {
+  tasks: Task[];
   constructor() {
     this.tasks = [
       new Task(
         1,
         "Task Creation UI",
         "Donec non sem sit amet mi hendrerit ultrices quis ac sem. Quisque vitae elit nunc...Donec non sem sit amet mi hendrerit ultrices quis ac sem. Quisque vitae elit nunc...Donec non sem sit amet mi hendrerit ultrices quis ac sem. Quisque vitae elit nunc...Donec non sem sit amet mi hendrerit ultrices quis ac sem. Quisque vitae elit nunc...Donec non sem sit amet mi hendrerit ultrices quis ac sem. Quisque vitae ",
-        "12/05/2025",
-        "14/05/2025",
+        new Date("12/05/2025"),
+        new Date("14/05/2025"),
         ["/avatars/user1.jpg", "/avatars/user2.jpg","/avatars/user3.jpg"],
         "On Progress"
       ),
@@ -16,8 +17,8 @@ export class TaskViewModel2 {
         2,
         "Calendar Integration",
         "Donec non sem sit amet mi hendrerit ultrices quis ac sem. Quisque vitae elit nunc...Donec non sem sit amet mi hendrerit ultrices quis ac sem. Quisque vitae elit nunc...Donec non sem sit amet mi hendrerit ultrices quis ac sem. Quisque vitae elit nunc...Donec non sem sit amet mi hendrerit ultrices quis ac sem. Quisque vitae elit nunc...Donec non sem sit amet mi hendrerit ultrices quis ac sem. Quisque vitae ",
-        "12/05/2025",
-        "14/05/2025",
+        new Date("12/05/2025"),
+        new Date("14/05/2025"),
         ["/avatars/user1.jpg", "/avatars/user2.jpg","/avatars/user3.jpg"],
         "On Hold"
       ),
@@ -32,7 +33,7 @@ export class TaskViewModel2 {
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
-  addTask(task) {
+  addTask(task : Task) {
     this.tasks.push(task);
   }
 }
