9b9f3e8182c52a9a0244dea79bbc437c5b4545f3
anugrahsivadasan-react <anugrahshivadasan@ziyaacademy.co.in>
2026-01-19 11:03:45 +0530

    test

--- src/views/Dashboard/DashComponents/ProfileCard.tsx
+++ src/views/Dashboard/DashComponents/ProfileCard.tsx
@@ -34,7 +34,7 @@ const DashboardHeader: React.FC = () => {
         />
         <div>
           <h2 className="text-[32px] font-bold text-[#4D4D4D] font-poppins">
-            Welcome Back,{" "}
+            Welcome ack,{" "}
             <span className="text-[#4D4D4D]">
               {profileLoading ? "Loading..." : userName}
             </span>
